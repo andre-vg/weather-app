39d184aa0a6d7119abb718d7011ee20eda13192d
andre.goncalves <andre.goncalves@grupoicts.com.br>
2023-03-02 00:39:16 -0300

    ✨ wow

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
-import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
-import { motion as m } from "framer-motion";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import { AnimatePresence, motion as m } from "framer-motion";
 
 function Info(props) {
   const { hora } = props;
   const valores = [];
   valores.push(props.info);
   return (
-    <m.div
+    <div
       initial={{
         opacity: 0,
         y: 100,
@@ -21,34 +21,56 @@ function Info(props) {
         duration: 0.5,
         ease: "easeInOut",
       }}
-      className="flex justify-center items-center h-[85%] flex-col font-semibold gap-3"
+      className="flex justify-center items-center h-[85%] flex-col font-semibold gap-6 lexend"
     >
-      <div className="px-6 text-3xl flex justify-between w-full">
-        <div className="flex gap-1">
-          <LocationOnRoundedIcon fontSize="large" />
-          <p className="text-3xl">
-            <span>{props.info.name}</span>
-          </p>
+      <div className="flex justify-center flex-col text-center">
+        <m.p
+          className="text-2xl text-neutral-900"
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 0.7 }}
+        >
+          <span>{props.info.name}</span>
+        </m.p>
+        <div className="flex justify-center">
+          <m.p
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.5, ease: "easeOut", duration: 1 }}
+            className="text-6xl"
+          >
+            {Math.round(props.info.main.temp)}
+          </m.p>
+          <m.p
+            initial={{ x: -40, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.6, ease: "easeOut", duration: 1 }}
+            className="text-6xl"
+          >
+            °
+          </m.p>
         </div>
-        <p className="text-3xl">
-          <span>{Math.round(props.info.main.temp)}</span>°C
-        </p>
-      </div>
-      <div className="px-6 text-3xl flex justify-between w-full">
-        <div className="flex gap-1">
-          <AccessTimeFilledRoundedIcon fontSize="large" />
-          <p>Local Time:</p>
+        <div className="flex gap-1 justify-center text-neutral-900">
+          <m.div
+            initial={{ x: -15, opacity: 0, rotate: -90 }}
+            animate={{ x: 0, opacity: 1, rotate: 0 }}
+            transition={{ delay: 1, ease: "easeOut", duration: 1 }}
+            className="self-center"
+          >
+            <AccessTimeOutlinedIcon fontSize="small" />
+          </m.div>
+          <p className="text-md">{hora}</p>
         </div>
-        <p>{hora}</p>
+        <p className="text-xl text-neutral-900">{props.info.weather[0].main}</p>
       </div>
-      <div className="px-6 text-3xl flex justify-center w-full">
-        <img src={`icons/${props.info.weather[0].icon}.png`} alt="icon" />
+
+      <div className="px-6 flex justify-center w-2/5">
+        <img
+          src={`icons/newIcons/${props.info.weather[0].icon}.png`}
+          alt="icon"
+        />
       </div>
-      <div className="flex justify-center w-full">
-        {props.info.weather[0].main}
-      </div>
-      {/* {props.info ? JSON.stringify(valores) : "Carregando..."} */}
-    </m.div>
+    </div>
   );
 }
 
